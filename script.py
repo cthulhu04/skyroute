0c2cfe4f142ff645f0321a2a0a68ee94536d6f7d
Nikolay Schekin <ns.datascientist@gmail.com>
2019-10-24 12:11:04 +0300

    Final commit

--- script.py
+++ script.py
@@ -3,7 +3,7 @@ from vc_metro import *
 from vc_landmarks import *
 from landmark_choices import *
 
-stations_under_constructions = ['Vancouver City Centre', 'Edmonds']
+stations_under_constructions = ['Vancouver City Centre', 'Edmonds', 'Waterfront', 'Granville']
 
 def skyroute():
 	greet()
@@ -14,6 +14,12 @@ def skyroute():
 def greet():
 	print("Hi there and welcome to SkyRoute!")
 	print("We'll help you find the shortest route between any of Vancouver landmarks.")
+
+def show_landmarks():
+	see_landmarks = input("Would you like to see the list of landmarks? Enter y/n: ")
+	if see_landmarks == 'y':
+		for landmark in landmark_choices:
+			print(landmark + ': ' + landmark_choices[landmark])
 
 def new_route(start_point=None, end_point=None):
 	start_point, end_point = set_start_and_end(start_point, end_point)
@@ -81,12 +87,6 @@ def get_route(start_point, end_point):
 
 	return min(routes, key=len)
 
-def show_landmarks():
-	see_landmarks = input("Would you like to see the list of landmarks? Enter y/n: ")
-	if see_landmarks == 'y':
-		for landmark in landmark_choices:
-			print(landmark + ': ' + landmark_choices[landmark])
-
 def update_metro_system(start_station, end_station):
 	if stations_under_constructions:
 		metro_system = get_active_stations()
@@ -109,27 +109,39 @@ def get_active_stations():
 def goodbye():
 	print('Thanks for using SkyRoute!')
 
+def add_or_remove_station():
+	print('Would you like to add or remove station?', end=' ')
+	choice = input('Please, enter "a" for adding and "r" for removing: ')
+	if choice == 'a':
+		add_station_under_construction()
+	elif choice == 'r':
+		remove_station_under_construction()
+	else:
+		print('The choice is invalid. Try again!')
+		add_or_remove_station()
+
 def add_station_under_construction():
 	station = input('Please, enter the station which is under construction now: ')
 	if is_valid_station(station):
 		stations_under_constructions.append(station)
+		again = input('Would you like to add another station? Enter y/n: ')
+		if again == 'y':
+			add_station_under_construction()
 	else:
 		print('It seems like this is not valid station. Please, try again!')
-		add_station_under_construction()
-	again = input('Would you like to add another station? Enter y/n')
-	if again == 'y':
 		add_station_under_construction()
 
 def remove_station_under_construction():
 	station = input('Please, enter the station which has been repared: ')
 	if is_valid_station(station):
 		stations_under_constructions.remove(station)
+		again = input('Would you like to remove another station? Enter y/n: ')
+		if again == 'y':
+			remove_station_under_construction()
 	else:
 		print('It seems like this is not valid station. Please, try again!')
 		remove_station_under_construction()
-	again = input('Would you like to add another station? Enter y/n')
-	if again == 'y':
-		remove_station_under_construction()
+
 
 def is_valid_station(station):
 	for landmark in vc_landmarks:
@@ -137,6 +149,3 @@ def is_valid_station(station):
 			if current_station == station:
 				return True
 	return False
-
-
-skyroute()
